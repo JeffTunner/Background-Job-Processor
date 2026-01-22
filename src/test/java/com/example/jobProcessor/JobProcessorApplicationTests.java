be1b0683981ba0612913de655c72cf5dbd9d452b
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-22 13:13:41 +0530

    Initial Commit

---
+++ src/test/java/com/example/jobProcessor/JobProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
