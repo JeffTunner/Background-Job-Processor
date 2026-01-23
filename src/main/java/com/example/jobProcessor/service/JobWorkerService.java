0fecba6bbec60ce999f405247e1485e67ce3024c
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-23 17:21:42 +0530

    Separated the picking of jobs and executing them.

--- src/main/java/com/example/jobProcessor/service/JobWorkerService.java
+++ src/main/java/com/example/jobProcessor/service/JobWorkerService.java
@@ -1,39 +1,31 @@
 package com.example.jobProcessor.service;
 
 import com.example.jobProcessor.entity.Job;
+import com.example.jobProcessor.entity.JobHistory;
 import com.example.jobProcessor.entity.Status;
+import com.example.jobProcessor.repository.JobHistoryRepository;
 import com.example.jobProcessor.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class JobWorkerService {
 
     @Autowired
-    JobService jobService;
-
-    @Autowired
     JobRepository jobRepository;
 
+    @Autowired
+    JobExecutor executor;
+
     @Scheduled(fixedDelay = 30000)
-    @Transactional
     public void pickJobs() {
+        System.out.println("Scheduler Triggered!");
         List<Job> jobs = jobRepository.findByStatus(Status.QUEUED);
-        jobs.forEach(job -> {
-            job.setStatus(Status.RUNNING);
-            jobRepository.save(job);
-            try {
-                jobService.executeJob(job);
-                job.setStatus(Status.COMPLETED);
-                jobRepository.save(job);
-            } catch (Exception e) {
-                job.setStatus(Status.FAILED);
-                jobService.handleRetry(job);
-            }
-        });
+        jobs.forEach(executor::processSingleJob);
     }
 }
