13f5c41be6a2564fda044469fa58403fe2184d71
JeffTunner <abhinavkaushik05@gmail.com>
2026-01-23 17:20:06 +0530

    Implemented the Job History by logging the status changes of job

--- src/main/java/com/example/jobProcessor/service/JobService.java
+++ src/main/java/com/example/jobProcessor/service/JobService.java
@@ -3,18 +3,25 @@ package com.example.jobProcessor.service;
 import com.example.jobProcessor.dto.JobRequestDto;
 import com.example.jobProcessor.dto.JobResponseDto;
 import com.example.jobProcessor.entity.Job;
+import com.example.jobProcessor.entity.JobHistory;
 import com.example.jobProcessor.entity.Status;
+import com.example.jobProcessor.repository.JobHistoryRepository;
 import com.example.jobProcessor.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 public class JobService {
 
     @Autowired
     JobRepository jobRepository;
+
+    @Autowired
+    JobHistoryRepository historyRepository;
 
     //DTO -> Entity
     private Job toEntity(JobRequestDto requestDto) {
@@ -34,6 +41,12 @@ public class JobService {
         job.setStatus(Status.QUEUED);
         job.setRetryCount(0);
         Job saved = jobRepository.save(job);
+        JobHistory history = new JobHistory();
+        history.setJobId(job.getId());
+        history.setStatus(Status.QUEUED);
+        history.setTimestamp(LocalDateTime.now());
+        history.setMessage("Job created!");
+        historyRepository.save(history);
         return toDto(saved);
     }
 
@@ -44,10 +57,22 @@ public class JobService {
             retry++;
             job.setRetryCount(retry);
             jobRepository.save(job);
+            JobHistory history = new JobHistory();
+            history.setStatus(Status.QUEUED);
+            history.setJobId(job.getId());
+            history.setMessage("Retrying after failing.");
+            history.setTimestamp(LocalDateTime.now());
+            historyRepository.save(history);
         } else {
-            job.setStatus(Status.FAILED);
+            job.setStatus(Status.DEAD);
             job.setRetryCount(retry);
             jobRepository.save(job);
+            JobHistory history = new JobHistory();
+            history.setTimestamp(LocalDateTime.now());
+            history.setJobId(job.getId());
+            history.setStatus(Status.DEAD);
+            history.setMessage("Job is Dead. No more retrying!");
+            historyRepository.save(history);
         }
     }
 
